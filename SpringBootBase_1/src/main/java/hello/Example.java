81f6910b31e7797c7060f158dc2b02a4b732c6d9
juanc.sabastizagal <juanc.sabastizagal@192.168.1.65>
2016-09-14 16:01:51 -0500

    second commit

--- SpringBootBase_1/src/main/java/hello/Example.java
+++ SpringBootBase_1/src/main/java/hello/Example.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class Example {
-
+//
 	@RequestMapping("/hello")
 	String hello()
 	{
